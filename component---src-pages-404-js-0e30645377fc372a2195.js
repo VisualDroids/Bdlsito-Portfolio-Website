00aa2aec971c323121a0d78f123fd8f084440420
mihgehl <mihgehl@users.noreply.github.com>
2023-05-23 04:12:37 +0000

    deploy: VisualDroids/Bdlsito-Portfolio-Website-Source@d49e5e9be5ab84dbaffaacd1b3aaca09f7f30421

---
+++ component---src-pages-404-js-0e30645377fc372a2195.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbdlsito_website=self.webpackChunkbdlsito_website||[]).push([[883],{429:function(e,t,s){s.r(t);var l=s(7294);t.default=()=>l.createElement("h1",null,"404 - Page Not Found")}}]);
+//# sourceMappingURL=component---src-pages-404-js-0e30645377fc372a2195.js.map
